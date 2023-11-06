9af9b5115bf8086793c09d2d7f3a93cd2d859812
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2023-11-07 01:45:29 +0530

    chapter-3

--- chapter-3/8-basic.tf
+++ chapter-3/8-basic.tf
@@ -3,3 +3,11 @@ variable "myvar" {
     description = "(optional) describe your variable"
     default = "hello terraform"
 }
+variable "mymap" {
+    type = map(string)
+    description = "(optional) describe your variable"
+    default = {
+        name = "Abhishek"
+        sir_name = "Srivastava"
+    }
+}
