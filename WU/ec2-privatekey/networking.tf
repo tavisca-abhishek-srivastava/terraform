4ce52261d8926dfdd33b058a377fcafb11599960
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2023-12-18 19:25:49 +0530

    added code to automate instance creation using tf

--- WU/ec2-privatekey/networking.tf
+++ WU/ec2-privatekey/networking.tf
@@ -1,6 +1,6 @@
-provider "aws" {
-region = "us-east-1"
-}
+# provider "aws" {
+# region = "us-east-1"
+# }
 
 resource "aws_security_group" "ec2_security_groups" {
     name   = var.name_security_groups
