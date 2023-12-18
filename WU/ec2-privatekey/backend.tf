9bef53b4aef20f563972b3e31e9d99a00174c978
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2023-12-18 19:24:13 +0530

    added code to automate instance creation using tf

--- WU/ec2-privatekey/backend.tf
+++ WU/ec2-privatekey/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
-    bucket = "terrafrom-ec2-privatekey-abhishek"
-    key = "terraform/remote-backend-ec2-privatekey"
+    bucket = "tapoc-ec2-privatekey"
+    key = "ta/remote-backend-ec2-privatekey"
     region = "us-east-1"
 
 
--- WU/ec2-privatekey/backendStore.tf
+++ WU/ec2-privatekey/backendStore.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "S3_For_RSS" {
 
-bucket = "terrafrom-ec2-privatekey-abhishek"
+bucket = "tapoc-ec2-privatekey"
 
 lifecycle {
             prevent_destroy = true
