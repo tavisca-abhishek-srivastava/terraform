46cc6bc772e9ddc43084a4008d7986d15ad82949
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2023-12-18 20:12:14 +0530

    added code to automate instance creation using tf

--- WU/ec2-privatekey/backendStore.tf
+++ WU/ec2-privatekey/backendStore.tf
@@ -3,8 +3,8 @@ resource "aws_s3_bucket" "S3_For_RSS_feed" {
 bucket = "tapoc-ec2-privatekey-1"
 
  tags = {
-            DataClassification: restricted
-            Environment: poc
+            DataClassification: "restricted"
+            Environment: "poc"
             AppName:  "as-testing-client-1-s33"
             InfraOwner: "sre-cloud-reliability@tavisca.com"
             BusinessUnit: "travel.app"
