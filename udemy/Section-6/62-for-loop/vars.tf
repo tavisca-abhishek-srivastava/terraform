d48b18a7b9ca0cca8aecfb70365c4865471805d8
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2023-12-29 11:34:21 +0530

    added code for mysql-rds

--- udemy/Section-6/62-for-loop/vars.tf
+++ udemy/Section-6/62-for-loop/vars.tf
@@ -1,19 +1,13 @@
-variable "list1" {
-    type = list(number)
-    default = [ 1,2,3,4,5,6 ]
-  
+variable "aws_region" {
+    type = string
+  default = "us-east-1"
 }
-variable "list2" {
-    type = list(string)
-    default = [ "apple","pear","mango","banana" ]
-    }
 
-variable "map1" {
-  type = map(number)
-  default = {
-    "apple" = 10,
-    "pear"= 20,
-    "banana" = 30,
-    "mango" = 40
-  }
+variable "project_tags" {
+    type = map(string)
+    default = {
+      "Component" = "FrontEnd",
+      "Environment" = "Production"
+    }
+  
 }
