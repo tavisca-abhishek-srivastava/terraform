87727bb3f26591f7155de5efbc487c3fd2077b40
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-02-05 08:32:42 +0530

    added code to pverride variable value

--- udemy-2/Section-6/64-for-each-loop-ec2/Projects/a/myec2.tf
+++ udemy-2/Section-6/64-for-each-loop-ec2/Projects/a/myec2.tf
@@ -1,11 +1,30 @@
 module "update-ami" {
     source = "../../modules/update-ami"
-    
 }
 
 
 module "ec2module" {
     
     source = "../../modules/ec2"
+    keyName = "mykey_test_fe_ec2"    #keyName is getting overridden
+    sg_egress_rules = {
+
+    rule1 = {
+        from_port  = 0
+        to_port    = 0
+        protocol   = "-1"
+        cidr_block = "0.0.0.0/0"
+        },
+    rule2 = {
+        from_port  = 0
+        to_port    = 0
+        protocol   = "-1"
+        cidr_block = "10.0.0.0/8"
+        },
+
+
+    }
+
+    
     
 }
