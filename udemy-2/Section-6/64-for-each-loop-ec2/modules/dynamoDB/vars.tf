399e6e74a0bbf63a8edf038d0b2155bf47b3e4ac
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-02-05 21:42:46 +0530

    added dynamoDB table module and called it

--- udemy-2/Section-6/64-for-each-loop-ec2/modules/dynamoDB/vars.tf
+++ udemy-2/Section-6/64-for-each-loop-ec2/modules/dynamoDB/vars.tf
@@ -2,3 +2,24 @@ variable "aws_dynamodb_table_name" {
   type = string
   default = "tf_bnr_testing"
 }
+
+variable "hash_key" {
+  default = "user_id"
+}
+
+variable "range_key" {
+  default = "product_id"
+}
+
+variable "other_attr" { 
+    type = map(object({
+      "name" = string
+      "type" = string
+    }))
+  default = {
+    "attr1" = { name = "user_id", type = "S" },
+    "attr2" = { name = "product_id", type = "S" },
+    "attr3" = { name = "product_name", type = "S" },
+    "attr4" = { name = "product_desc", type = "S" },
+  }
+}
