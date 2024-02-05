d3807ec8f1404bf6dca0f47d68482424ba2c0412
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-02-05 13:40:59 +0530

    added dynamoDB table module and called it

---
+++ udemy-2/Section-6/64-for-each-loop-ec2/modules/dynamoDB/dynamoDB.tf
@@ -0,0 +1,20 @@
+resource "aws_dynamodb_table" "ddtable_plain" {
+  name = "${var.aws_dynamodb_table_name}"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 30
+  write_capacity = 30
+  
+
+
+
+  tags =   {
+    DataClassification : "restricted"
+    Environment : "poc"
+    AppName : "tf-nrt-${var.aws_dynamodb_table_name}"
+    InfraOwner : "sre-cloud-reliability@tavisca.com"
+    BusinessUnit : "travel.app"
+    Backup : "no"
+    Product : "poap"
+    Name : "tf-${var.aws_dynamodb_table_name}"
+  }
+}
