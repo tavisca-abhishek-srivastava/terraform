5f0fc54a6e13c1c778a1028ef59240d6093fc9ce
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-02-05 11:14:43 +0530

    commented provider block in ec2 module

--- udemy-2/Section-6/64-for-each-loop-ec2/Projects/a/backend.tf
+++ udemy-2/Section-6/64-for-each-loop-ec2/Projects/a/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tapoc-ec2-privatekey"
-    key    = "ta/remote-backend-module-multiple-ec2"
+    key    = "ta/remote-backend-module-nrt-multiple-ec2"
     region = "us-east-1"
 
 
--- udemy-2/Section-6/64-for-each-loop-ec2/Projects/a/myec2.tf
+++ udemy-2/Section-6/64-for-each-loop-ec2/Projects/a/myec2.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      version = "5.32.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-
 module "update-ami" {
     source = "../../modules/update-ami"
 }
