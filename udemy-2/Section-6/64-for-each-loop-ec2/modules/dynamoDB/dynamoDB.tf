05ffa223318d825f250b7a917db3b729026b361b
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-02-05 23:10:50 +0530

    added dynamoDB table, GSI module and called it

--- udemy-2/Section-6/64-for-each-loop-ec2/modules/dynamoDB/dynamoDB.tf
+++ udemy-2/Section-6/64-for-each-loop-ec2/modules/dynamoDB/dynamoDB.tf
@@ -6,29 +6,24 @@ resource "aws_dynamodb_table" "ddtable_plain" {
   hash_key = var.hash_key
   range_key = var.range_key
   
-#     global_secondary_index {
-#         name               = "product_id-user_id-index"
-#         hash_key           = "product_id"
-#         range_key          = "user_id"
-#         write_capacity     = 10
-#         read_capacity      = 10
-#         projection_type    = "ALL"
+    global_secondary_index {
+        name               = "product_id-user_id-index"
+        hash_key           = "product_id"
+        range_key          = "user_id"
+        write_capacity     = 10
+        read_capacity      = 10
+        projection_type    = "ALL"
         
-#   }
+  }
 
-    for_each = var.other_attr
-        attribute {
-        name = each.value.name
-        type = each.value.type
-        }
-    # attribute {
-    #   name = "user_id"
-    #   type = "S"
-    # }
-    # attribute {
-    #   name = "product_id"
-    #   type = "S"
-    # }
+    attribute {
+      name = var.hash_key
+      type = "S"
+    }
+    attribute {
+      name = var.range_key
+      type = "S"
+    }
     
 
 
