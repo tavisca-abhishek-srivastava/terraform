e4ea3c0a881b4b119b9b7bee6680c83304f47011
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-02-06 09:15:16 +0530

    added dynamoDB table, GSI module and called it

---
+++ udemy-2/Section-6/64-for-each-loop-ec2/Projects/dynamoDB/nrt-dynamoDB.tf
@@ -0,0 +1,19 @@
+module "nrt-dynamoDB" {
+    source = "../../modules/dynamoDB"
+    
+
+
+    # attribute {
+    #   name = var.hash_key
+    #   type = "S"
+    # }
+    # attribute {
+    #   name = var.range_key
+    #   type = "S"
+    # }
+    # attribute {
+    #   name = "road_id"
+    #   type = "S"
+    # }
+
+}
--- udemy-2/Section-6/64-for-each-loop-ec2/modules/dynamoDB/dynamoDB.tf
+++ udemy-2/Section-6/64-for-each-loop-ec2/modules/dynamoDB/dynamoDB.tf
@@ -6,45 +6,29 @@ resource "aws_dynamodb_table" "ddtable_plain" {
   hash_key = var.hash_key
   range_key = var.range_key
   
-    global_secondary_index {
-        name               = "product_id-user_id-index"
-        hash_key           = "product_id"
-        range_key          = "user_id"
-        write_capacity     = 10
-        read_capacity      = 10
-        projection_type    = "ALL"
-        
+   dynamic "global_secondary_index" {
+   for_each = local.indices
+    content {
+      name            = global_secondary_index.key
+      write_capacity  = global_secondary_index.value.write_capacity
+      read_capacity   = global_secondary_index.value.read_capacity
+      range_key       = global_secondary_index.value.range_key
+      hash_key        = global_secondary_index.key
+      projection_type = "ALL"
+    }
   }
-      global_secondary_index {
-        name               = "road_id-user_id-index"
-        hash_key           = "road_id"
-        range_key          = "user_id"
-        write_capacity     = 10
-        read_capacity      = 10
-        projection_type    = "ALL"
-        
-  }
-
-
-    for_each = var.gsi
-       attribute {
-        name = "${each.value.name}"
-        type = "${each.value.type}"
-   }
-
-    # attribute {
-    #   name = var.hash_key
-    #   type = "S"
-    # }
-    # attribute {
-    #   name = var.range_key
-    #   type = "S"
-    # }
-    # attribute {
-    #   name = "road_id"
-    #   type = "S"
-    # }
-
+    attribute {
+      name = var.hash_key
+      type = "S"
+    }
+    attribute {
+      name = var.range_key
+      type = "S"
+    }
+    attribute {
+      name = "road_id"
+      type = "S"
+    }
 
 
   tags =   {
