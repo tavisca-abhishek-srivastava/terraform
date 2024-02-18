ff51336a25dd83d247033a7e22228d88003db4f2
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-02-18 23:38:02 +0530

    added autoscaling for provisioned table

--- actual-work/Projects/NRT-dynamoDB/main.tf
+++ actual-work/Projects/NRT-dynamoDB/main.tf
@@ -1,4 +1,4 @@
 module "nrt-dynamoDB" {
-    source = "../../modules/dynamoDB/DD_Table_PAY_PER_REQUEST"
+    source = "../../modules/dynamoDB/DD_Table_Provisioned"
     table_hash_key = var.table_hash_key
 }
