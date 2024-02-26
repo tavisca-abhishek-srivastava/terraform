6a04375ae20907514ea74966ec9d5e981223dfea
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-02-26 13:46:25 +0530

    changed autoscaling value

--- actual-work/modules/dynamoDB/DD_Table_Provisioned/autoscaling_indexes.tf
+++ actual-work/modules/dynamoDB/DD_Table_Provisioned/autoscaling_indexes.tf
@@ -27,17 +27,18 @@ resource "aws_appautoscaling_policy" "environment_table_by_geo_location_read_pol
 resource "aws_appautoscaling_target" "environment_table_by_geo_location_write_target" {
   max_capacity       = 10
   min_capacity       = 5
+  for_each = var.gsi_indices
   resource_id        = "table/${aws_dynamodb_table.DD_Table_Provisioned.name}/index/${each.key}"
   scalable_dimension = "dynamodb:index:WriteCapacityUnits"
   service_namespace  = "dynamodb"
 }
 
 resource "aws_appautoscaling_policy" "environment_table_by_geo_location_write_policy" {
-  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.environment_table_by_geo_location_write_target.resource_id}"
+  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.environment_table_by_geo_location_write_target[each.key].resource_id}"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.environment_table_by_geo_location_write_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.environment_table_by_geo_location_write_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.environment_table_by_geo_location_write_target.service_namespace
+  resource_id        = aws_appautoscaling_target.environment_table_by_geo_location_write_target[each.key].resource_id
+  scalable_dimension = aws_appautoscaling_target.environment_table_by_geo_location_write_target[each.key].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.environment_table_by_geo_location_write_target[each.key].service_namespace
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
