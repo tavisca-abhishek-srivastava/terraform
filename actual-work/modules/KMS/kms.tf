257eab7069f5a82344a3c282b51b8ba90ba90127
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-05 14:43:18 +0530

    new dest dd table

--- actual-work/modules/KMS/kms.tf
+++ actual-work/modules/KMS/kms.tf
@@ -1,11 +1,12 @@
 resource "aws_kms_key" "dynamodb_encryption_key" {
-for_each = (var.need_cmk == false ? [] : [1])
+for_each = var.need_cmk == false ? [] : [1]
   key_usage                = "ENCRYPT_DECRYPT"
   description              = var.key_description
   deletion_window_in_days  = var.delete_after_days
   customer_master_key_spec = "SYMMETRIC_DEFAULT"
   enable_key_rotation      = true
   multi_region             = true
+  
   tags = {
     DataClassification : "restricted"
     Environment : "poc"
@@ -19,14 +20,14 @@ for_each = (var.need_cmk == false ? [] : [1])
 }
 
 resource "aws_kms_key_policy" "dd_table_key_policy" {
-for_each = (var.need_cmk ==  false ? [] : [1] )
+for_each = var.need_cmk ==  false ? [] : [1] 
   key_id = aws_kms_key.dynamodb_encryption_key.key_id
   policy = jsonencode(var.key_policy_map)
 
 
 }
 resource "aws_kms_alias" "key_alias" {
-for_each = (var.need_cmk ==  false ? [] : [1] )
+for_each = var.need_cmk ==  false ? [] : [1]
 #   name          = "alias/nrt_encryption_key"
     name = var.kms_alias
     target_key_id = aws_kms_key.dynamodb_encryption_key.key_id
