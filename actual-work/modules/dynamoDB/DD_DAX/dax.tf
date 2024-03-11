5290b5b8bdfc2e11a1ed4c6c77b19e53c54caac0
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-11 10:45:55 +0530

    added code for db_subnet_group and changed cluster name

--- actual-work/modules/dynamoDB/DD_DAX/dax.tf
+++ actual-work/modules/dynamoDB/DD_DAX/dax.tf
@@ -1,6 +1,8 @@
 resource "aws_db_subnet_group" "dax_subnet_group" {
   name = var.subnet_group_details.name
   subnet_ids = [var.subnet_group_details.subnet_id_1,var.subnet_group_details.subnet_id_2,var.subnet_group_details.subnet_id_3]
+  tags = var.tags
+
 }
 
 resource "aws_dax_cluster" "bar" {
