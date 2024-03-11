522421be7609c70df817816df4d4cbbbb7ebe987
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-11 21:03:09 +0530

    added code for db_subnet_group and changed cluster name

--- actual-work/modules/dynamoDB/DD_DAX/dax.tf
+++ actual-work/modules/dynamoDB/DD_DAX/dax.tf
@@ -12,7 +12,7 @@ resource "aws_dax_cluster" "bar" {
     cluster_endpoint_encryption_type = var.cluster_endpoint_encryption_type
     replication_factor = var.replication_factor
     parameter_group_name = var.parameter_group_name
-    subnet_group_name = aws_db_subnet_group.dax_subnet_group.id
+    subnet_group_name = aws_dax_subnet_group.dax_subnet_group.id
     server_side_encryption {
       enabled = true
                             }
