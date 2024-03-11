1af3f457bf6ae9fcfec4941ddef2f49c0e9abc2f
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-11 09:22:54 +0530

    added code for db_subnet_group

--- actual-work/modules/dynamoDB/DD_DAX/dax.tf
+++ actual-work/modules/dynamoDB/DD_DAX/dax.tf
@@ -1,3 +1,10 @@
+resource "aws_db_subnet_group" "dax_subnet_group" {
+  name = var.subnet_group_details.value.name
+  subnet_ids = [subnet_group_details.value.subnet_id_1,subnet_group_details.value.subnet_id_2,subnet_group_details.value.subnet_id_3]
+}
+
+
+
 resource "aws_dax_cluster" "bar" {
     cluster_name       = var.cluster_name
     description = var.cluster_description
@@ -6,7 +13,7 @@ resource "aws_dax_cluster" "bar" {
     cluster_endpoint_encryption_type = var.cluster_endpoint_encryption_type
     replication_factor = var.replication_factor
     parameter_group_name = var.parameter_group_name
-    subnet_group_name = var.subnet_group
+    subnet_group_name = aws_db_subnet_group.dax_subnet_group.name
 
   server_side_encryption {
     enabled = true
