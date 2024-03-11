cc797c35d26e2391dc941dbc647ce68a9a9010b9
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-11 09:22:54 +0530

    added code for db_subnet_group

--- actual-work/modules/dynamoDB/DD_DAX/vars.tf
+++ actual-work/modules/dynamoDB/DD_DAX/vars.tf
@@ -3,7 +3,8 @@ variable "cluster_name" {
     type = string
 }
 variable "iam_role_arn" {
-  type = string
+    description = "IAM service role to connect to DyanmoDB. -> https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DAX.create-cluster.cli.create-service-role.html"
+    type = string
 }
 variable "node_type" {
     description = "Choose the type of nodes to run in your clusters. All nodes in each cluster must be of the same type. You cannot modify the node types for a running DAX cluster."
@@ -15,22 +16,36 @@ variable "replication_factor" {
 }
 
 variable "cluster_endpoint_encryption_type" {
-    description = "value"
+    description = "provide NONE or TLS"
     type = string
-  default = "NONE"
+    default = "NONE"
 }
 variable "cluster_description" {
-  type = string
+    description = "enter meaningful description for the usages of DAX cluster "
+    type = string
 }
 variable "parameter_group_name" {
-  type = string
+    description = "enter existing parameter group"
+    type = string
 }
 
 variable "security_group_ids" {
+    description = "enter existing security group"
   type = string
 }
 
-variable "subnet_group" {
-    type = string
+variable "subnet_group_details" {
+    description = "enter subnet group details"
+    type = map(object({
+      name = string
+      subnet_id_1 = string
+      subnet_id_2 = string
+      subnet_id_3 = string
+    }))
+
+    validation {
+      condition = length(var.subnet_group_details) != 4
+      error_message = "please specify subnet group name and atleast 3 subnet ids"
+    }
   
 }
