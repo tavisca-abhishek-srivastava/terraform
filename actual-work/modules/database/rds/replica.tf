025970d8a917c5c5cba2f904b9e69a66498450f2
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-19 19:23:39 +0530

    added replica count 2

--- actual-work/modules/database/rds/replica.tf
+++ actual-work/modules/database/rds/replica.tf
@@ -8,7 +8,7 @@ resource "aws_db_instance" "read-only-cross-region" {
   availability_zone = "us-east-1b"
   identifier  = "jpmc-ro-${count.index}"
   apply_immediately = true
-  replicate_source_db  = aws_db_instance.rds_instance.arn
+  replicate_source_db  = aws_db_instance.rds_instance.identifier
   storage_encrypted = true
   kms_key_id = module.rds_storage_cmk.mrk_cms_arn
   #db_subnet_group_name = "default"
