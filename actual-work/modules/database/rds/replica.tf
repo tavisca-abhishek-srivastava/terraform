613aa4a60fd39a5a13741eca69348aa96a4ee3f8
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-19 16:55:20 +0530

    added replica count 2

--- actual-work/modules/database/rds/replica.tf
+++ actual-work/modules/database/rds/replica.tf
@@ -1,11 +1,12 @@
 resource "aws_db_instance" "read-only-cross-region" {
+  count = 2
   allocated_storage    = var.allocated_storage
   instance_class       = var.db_instance_class
 
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   availability_zone = "us-east-1b"
-  identifier  = "jpmc-ro"
+  identifier  = "jpmc-ro-${count.index}"
   apply_immediately = true
   replicate_source_db  = aws_db_instance.rds_instance.arn
   #db_subnet_group_name = "default"
