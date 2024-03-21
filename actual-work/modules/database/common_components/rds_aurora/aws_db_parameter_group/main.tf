6f26b69add22c1305ba50f0a5e23ff0fccd8a08c
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-21 11:52:35 +0530

    changed mysql parameter group family and added lifecycle policy

--- actual-work/modules/database/common_components/rds_aurora/aws_db_parameter_group/main.tf
+++ actual-work/modules/database/common_components/rds_aurora/aws_db_parameter_group/main.tf
@@ -11,6 +11,8 @@ resource "aws_db_parameter_group" "parameter_group_for_db" {
        value = parameter.value.value
     }
   }
-   
+   lifecycle {
+     create_before_destroy = true
+   }
     tags = var.tags
 }
