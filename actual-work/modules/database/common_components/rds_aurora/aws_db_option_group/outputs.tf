e4fe9667729fe8c25b14e10ec0eed25384bc9463
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-24 23:22:22 +0530

    changed the logic for performance insight

--- actual-work/modules/database/common_components/rds_aurora/aws_db_option_group/outputs.tf
+++ actual-work/modules/database/common_components/rds_aurora/aws_db_option_group/outputs.tf
@@ -1,4 +1,3 @@
 output "option_group_name_output" {
-  value       = aws_db_option_group.option_group_for_db.arn
-  description = "option group name"
+  value       = aws_db_option_group.option_group_for_db
 }
