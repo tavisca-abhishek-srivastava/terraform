d972c2463ca2f88c008ed26a8ba1860b5beab605
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-24 23:15:28 +0530

    changed the logic for performance insight

--- actual-work/modules/database/common_components/rds_aurora/aws_db_parameter_group/outputs.tf
+++ actual-work/modules/database/common_components/rds_aurora/aws_db_parameter_group/outputs.tf
@@ -1,4 +1,3 @@
 	output "parameter_group_name" {
-	value = aws_db_parameter_group.parameter_group_for_db.name
-	description = "parameter group name"
+	value = aws_db_parameter_group.parameter_group_for_db.id
 	 }
