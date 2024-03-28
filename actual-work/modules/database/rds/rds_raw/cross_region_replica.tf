282a25b8c337394190e55f74f01aa7a7e990b995
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-28 13:36:19 +0530

    added corss region replica

--- actual-work/modules/database/rds/rds_raw/cross_region_replica.tf
+++ actual-work/modules/database/rds/rds_raw/cross_region_replica.tf
@@ -13,7 +13,7 @@ resource "aws_db_instance" "cross_region_read-only-replica" {
   availability_zone = var.az_for_cross_region_read_replica[count.index]
   identifier  = "jpmc-ro-${var.region_for_cross_region_read_replica}-${count.index}"
   apply_immediately = true
-  replicate_source_db  = aws_db_instance.rds_instance.identifier
+  replicate_source_db  = aws_db_instance.rds_instance.arn
   storage_encrypted = true
   kms_key_id = module.rds_storage_cmk.mrk_cms_arn
   max_allocated_storage = (var.enable_storage_autoscaling == true) ? var.max_allocated_storage:0
