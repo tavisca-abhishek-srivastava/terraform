35ab0e8061b6c1c81804b43682895a71a6752d81
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-28 13:20:47 +0530

    added corss region replica

--- actual-work/modules/database/rds/rds_raw/cross_region_replica.tf
+++ actual-work/modules/database/rds/rds_raw/cross_region_replica.tf
@@ -3,12 +3,6 @@ region= var.region_for_cross_region_read_replica
 alias = "dr"
 }
 
-
-
-
-
-
-
 resource "aws_db_instance" "cross_region_read-only-replica" {
   count = var.number_of_cross_region_read_replica
   instance_class       = var.db_instance_class
@@ -24,5 +18,6 @@ resource "aws_db_instance" "cross_region_read-only-replica" {
   kms_key_id = module.rds_storage_cmk.mrk_cms_arn
   max_allocated_storage = (var.enable_storage_autoscaling == true) ? var.max_allocated_storage:0
   tags = var.tags
+  provider = aws.dr
 
 }
