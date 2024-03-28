48e4cf629e4c81463067d49337f239a3af9fcba1
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-28 12:05:04 +0530

    added corss region replica

--- actual-work/modules/database/rds/rds_raw/cross_region_replica.tf
+++ actual-work/modules/database/rds/rds_raw/cross_region_replica.tf
@@ -3,11 +3,18 @@ region= var.region_for_cross_region_read_replica
 alias = "dr"
 }
 
+
+
+
+
+
+
 resource "aws_db_instance" "cross_region_read-only-replica" {
   count = var.number_of_cross_region_read_replica
   instance_class       = var.db_instance_class
   
   parameter_group_name = "default.mysql5.7"
+  db_subnet_group_name = var.db_subnet_group
   skip_final_snapshot  = true
   availability_zone = var.az_for_cross_region_read_replica[count.index]
   identifier  = "jpmc-ro-${var.region_for_cross_region_read_replica}-${count.index}"
