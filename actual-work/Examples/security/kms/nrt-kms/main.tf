c6e2b2640331d165525d81506c906b363ae43c91
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-01 00:31:17 +0530

    added code for kms replica in rds module

--- actual-work/Examples/security/kms/nrt-kms/main.tf
+++ actual-work/Examples/security/kms/nrt-kms/main.tf
@@ -5,6 +5,7 @@ module "nrt-dax" {
     key_description   = var.key_description
     key_policy_map    = var.key_policy_map
     replica_region = var.replica_region
+    need_kms_replica = var.need_kms_replica
     kms_tags = var.kms_tags
     replica_key_policy = var.replica_key_policy
     
