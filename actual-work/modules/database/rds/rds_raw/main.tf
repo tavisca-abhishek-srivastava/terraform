a89b63e11ce2c7a589eb5740c92437d2aa27e5e5
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-31 17:32:15 +0530

    added code for kms replica in rds module

--- actual-work/modules/database/rds/rds_raw/main.tf
+++ actual-work/modules/database/rds/rds_raw/main.tf
@@ -4,6 +4,9 @@
   	delete_after_days = var.delete_after_days
   	key_description   = var.key_description
   	key_policy_map    = var.key_policy_map
+	need_kms_replica = var.need_kms_replica
+	replica_region = var.replica_region
+	replica_key_policy = var.replica_key_policy
 	kms_tags = var.kms_tags
 }
 
