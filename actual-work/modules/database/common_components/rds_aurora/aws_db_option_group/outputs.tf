495c682bd3b9aefd757209aa0d594582f6b8e1c7
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-01 08:47:46 +0530

    added code for kms replica in rds module

--- actual-work/modules/database/common_components/rds_aurora/aws_db_option_group/outputs.tf
+++ actual-work/modules/database/common_components/rds_aurora/aws_db_option_group/outputs.tf
@@ -2,5 +2,5 @@ output "option_group_name_output" {
   value       = aws_db_option_group.option_group_for_db.id
 }
 output "my-string" {
-  value = random_string.option_group_name_postfix.result
+  value = "var.rds_option_group_name-${random_string.option_group_name_postfix.result}"
 }
