6e3da1304c076d67aa1426656fdfd655db7e2097
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-01 08:33:04 +0530

    added code for kms replica in rds module

--- actual-work/Examples/database/common_components/rds_aurora/aws_db_option_group_mysql/main.tf
+++ actual-work/Examples/database/common_components/rds_aurora/aws_db_option_group_mysql/main.tf
@@ -1,4 +1,4 @@
-module "nrt_rds_parameter_group" {
+module "nrt_rds_option_group" {
     source = "../../../../../modules/database/common_components/rds_aurora/aws_db_option_group"
     rds_option_group_name = var.rds_option_group_name
     option_group_engine_name = var.option_group_engine_name
