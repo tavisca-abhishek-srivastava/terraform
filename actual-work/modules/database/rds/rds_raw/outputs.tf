38515f59a2878d80da43a2b398c4ec667a933d9d
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-02 15:02:31 +0530

    added arn of ro replicas in output.tf

--- actual-work/modules/database/rds/rds_raw/outputs.tf
+++ actual-work/modules/database/rds/rds_raw/outputs.tf
@@ -6,3 +6,13 @@
 	value = aws_db_instance.read-only-replica[*].arn
 	description = "ARN of read only replica"
 	 }
+
+	output "rds_endpoint" {
+	value = aws_db_instance.rds_instance.endpoint
+	description = "ARN of primary instance"
+	 }
+	output "rds_read_only_replica_endpoint" {
+	value = aws_db_instance.read-only-replica[*].endpoint
+	description = "ARN of read only replica"
+	 }
+
