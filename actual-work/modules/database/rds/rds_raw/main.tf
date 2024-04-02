44f85427b2d247342045189209252e55bbdbeccd
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-02 13:02:00 +0530

    added module for subnet group

--- actual-work/modules/database/rds/rds_raw/main.tf
+++ actual-work/modules/database/rds/rds_raw/main.tf
@@ -37,7 +37,7 @@
 	  source = "../../common_components/rds_aurora/aws_db_subnet_group/"
 	  # if var.use_default_subnet_group == false then it will create new subnet group else will use default subnet group provided by user
 	  for_each = (var.use_default_subnet_group == false) ? toset(["1"]):toset([])
-	  subnet_group_name = var.db_subnet_group
+	  subnet_group_name = var.rds_subnet_group_name
 	  subnet_ids = var.subnet_group_subnet_ids
 	  tags = var.tags
 	}
