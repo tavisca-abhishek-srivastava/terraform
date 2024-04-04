5e782612a28cf008ab9c47e19c206a094bcc0626
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-05 02:09:56 +0530

    added pitr code

---
+++ actual-work/modules/database/common_components/rds_aurora/aws_db_subnet_group/locals.tf
@@ -0,0 +1,7 @@
+locals {
+    random_string_subnet_group_name_postfix_length = 8
+    random_string_subnet_group_name_postfix_special = false
+    random_string_subnet_group_name_postfix_numeric = false
+    random_string_subnet_group_name_postfix_upper = false
+    random_string_subnet_group_name_postfix_lower = true
+}
--- actual-work/modules/database/common_components/rds_aurora/aws_db_subnet_group/main.tf
+++ actual-work/modules/database/common_components/rds_aurora/aws_db_subnet_group/main.tf
@@ -7,10 +7,19 @@ terraform {
   }
 }
 
+resource "random_string" "subnet_group_name_postfix" {
+  length = local.random_string_subnet_group_name_postfix_length
+  special = local.random_string_subnet_group_name_postfix_special
+  numeric = local.random_string_subnet_group_name_postfix_numeric
+  upper = local.random_string_subnet_group_name_postfix_upper
+  lower = local.random_string_subnet_group_name_postfix_lower
+}
 resource "aws_db_subnet_group" "subnet_group_for_db" {
-  name = var.subnet_group_name
+  name = "${var.subnet_group_name}-${random_string.subnet_group_name_postfix.result}"
   subnet_ids = var.subnet_ids
   tags = var.tags
-
+  lifecycle {
+      create_before_destroy = true
+  }
 }
 
