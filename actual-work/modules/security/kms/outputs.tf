ae64d851448711f4caff13584621af903060ad8b
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-04 07:56:22 +0530

    module for same region ro replica

--- actual-work/modules/security/kms/outputs.tf
+++ actual-work/modules/security/kms/outputs.tf
@@ -3,5 +3,9 @@ value = aws_kms_key.dynamodb_encryption_key.arn
 }
 
 output "replica_mrk_cms_arn" {
-value = aws_kms_replica_key.replica[*].arn
+# value = aws_kms_replica_key.replica[*].arn
+value = [
+    for x in aws_kms_replica_key.replica:
+      x.arn
+  ]
 }
