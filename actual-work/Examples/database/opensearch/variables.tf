7ca40fdb6abce104a7f76da9af964306184ef7f1
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-05 15:30:39 +0530

    added code for opensearch

--- actual-work/Examples/database/opensearch/variables.tf
+++ actual-work/Examples/database/opensearch/variables.tf
@@ -44,49 +44,98 @@ variable "zone_awareness_enabled" {
 }
 
 variable "subnet_ids" {
+  description = "List of VPC Subnet IDs for the OpenSearch domain endpoints to be created in"
   type = list
 }
 
 variable "security_options_enabled" {
+  description = "Whether advanced security is enabled."
   type = bool
 }
 variable "security_group_ids" {
+  description = "List of VPC Security Group IDs to be applied to the OpenSearch domain endpoints. If omitted, the default Security Group for the VPC will be used"
   type = list
 }
-
-variable "master_user" {
-  type = string
+variable "internal_user_database_enabled" {
+  description = "Whether the internal user database is enabled. Default is false"
+  type = bool
+  default = false
 }
 
-variable "encrypt_at_rest_enabled" {
+variable "anonymous_auth_enabled" {
+  description = <<EOF
+  "Whether Anonymous auth is enabled. Enables fine-grained access control on an existing domain. 
+  Ignored unless advanced_security_options are enabled. Can only be enabled on an existing domain."
+  EOF
   type = bool
 }
 
+variable "master_user_name" {
+   description = <<EOF
+  "Main user's username, which is stored in the Amazon OpenSearch Service domain's internal database. 
+  Only specify if internal_user_database_enabled is set to true"
+  EOF
+  type = string
+}
+
+variable "master_user_password" {
+  description = <<EOF
+  "Main user's password, which is stored in the Amazon OpenSearch Service domain's internal database. 
+  Only specify if internal_user_database_enabled is set to true"
+  EOF
+
+}
+
+variable "encrypt_at_rest_enabled" {
+  description = <<EOF
+  "(Required) Whether to enable encryption at rest. If the encrypt_at_rest block is not provided then this defaults to false. 
+  Enabling encryption on new domains requires an engine_version of OpenSearch_X.Y or Elasticsearch_5.1 or greater"
+  EOF
+  type = bool
+  default = false
+}
+
 variable "domain_endpoint_options_enforce_https" {
+  description = "Whether or not to require HTTPS for domain endpoint"
   type = bool
 }
 
 variable "ebs_enabled" {
+  description = "Whether EBS volumes are attached to data nodes in the domain"
   type = bool
+  default = true
 }
 
 variable "ebs_volume_size" {
+  description = "(Required if ebs_enabled is set to true.) Size of EBS volumes attached to data nodes (in GiB)"
   type = number
 }
 
 variable "volume_type" {
+  description = "(Optional) Type of EBS volumes attached to data nodes"
   type = string
+  default = "gp3"
 }
 
 variable "throughput" {
+  description = <<EOF
+  "(Required if volume_type is set to gp3) Specifies the throughput (in MiB/s) of the EBS volumes attached to data nodes. 
+  Applicable only for the gp3 volume type."
+  EOF
   type = number
 }
 
+variable "ebs_iops" {
+  description = "(Optional) Baseline input/output (I/O) performance of EBS volumes attached to data nodes. Applicable only for the GP3 and Provisioned IOPS EBS volume types."
+  type = number
+}
 variable "security_group_ids" {
+  description = " List of VPC Security Group IDs to be applied to the OpenSearch domain endpoints. If omitted, the default Security Group for the VPC will be used"
   type = string
 }
 
 variable "aws_region" {
+  description = "region where opensearch domain will be created"
   type = string
 }
 
