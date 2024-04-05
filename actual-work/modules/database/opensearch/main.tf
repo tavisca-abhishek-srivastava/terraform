5c6a1382145dceaad589f64cece41112bd043a33
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-05 16:21:34 +0530

    added code for opensearch

--- actual-work/modules/database/opensearch/main.tf
+++ actual-work/modules/database/opensearch/main.tf
@@ -50,19 +50,6 @@ resource "aws_opensearch_domain" "opensearch" {
     iops = var.ebs_iops
   }
 
-  log_publishing_options {
-    cloudwatch_log_group_arn = aws_cloudwatch_log_group.opensearch_log_group_index_slow_logs.arn
-    log_type                 = "INDEX_SLOW_LOGS"
-  }
-  log_publishing_options {
-    cloudwatch_log_group_arn = aws_cloudwatch_log_group.opensearch_log_group_search_slow_logs.arn
-    log_type                 = "SEARCH_SLOW_LOGS"
-  }
-  log_publishing_options {
-    cloudwatch_log_group_arn = aws_cloudwatch_log_group.opensearch_log_group_es_application_logs.arn
-    log_type                 = "ES_APPLICATION_LOGS"
-  }
-
   node_to_node_encryption {
     enabled = true
   }
