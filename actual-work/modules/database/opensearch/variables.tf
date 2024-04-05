82169b121968d934b09ae30c20dc4166d2d32425
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-05 16:17:31 +0530

    added code for opensearch

--- actual-work/modules/database/opensearch/variables.tf
+++ actual-work/modules/database/opensearch/variables.tf
@@ -134,14 +134,6 @@ variable "aws_region" {
   type = string
 }
 
-variable "cloud_watch_log_retention_in_days" {
-  description = <<EOF
-  "Number of days cloud watch log will be retained.
-  Expected retention_in_days to be one of [0 1 3 5 7 14 30 60 90 120 150 180 365 400 545 731 1096 1827 2192 2557 2922 3288 3653] "
-  EOF
-  type = number
-  default = 14
-}
 variable "tags" {
   type = object({
     DataClassification = string
