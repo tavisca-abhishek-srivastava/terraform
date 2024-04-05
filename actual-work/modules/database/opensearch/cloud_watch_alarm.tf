5bb9b0d60a5f046fda828a5fc244e1a6c21eba22
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-05 16:03:46 +0530

    added code for opensearch

--- actual-work/modules/database/opensearch/cloud_watch_alarm.tf
+++ actual-work/modules/database/opensearch/cloud_watch_alarm.tf
@@ -40,16 +40,19 @@ CONFIG
 resource "aws_cloudwatch_log_group" "opensearch_log_group_index_slow_logs" {
   name              = "/aws/opensearch/${var.open_search_domain_name}/index-slow"
   retention_in_days = var.cloud_watch_log_retention_in_days
+  tags = var.tags
 }
 
 
 resource "aws_cloudwatch_log_group" "opensearch_log_group_search_slow_logs" {
   name              = "/aws/opensearch/${var.open_search_domain_name}/search-slow"
   retention_in_days = var.cloud_watch_log_retention_in_days
+  tags = var.tags
 }
 
 
 resource "aws_cloudwatch_log_group" "opensearch_log_group_es_application_logs" {
   name              = "/aws/opensearch/${var.open_search_domain_name}/es-application"
   retention_in_days = var.cloud_watch_log_retention_in_days
+  tags = var.tags
 }
