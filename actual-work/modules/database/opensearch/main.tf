1ea23d220907a7fa7837c2c55ea9bd481d1a7ffb
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-15 11:35:18 +0530

    added tag for opensearch

--- actual-work/modules/database/opensearch/main.tf
+++ actual-work/modules/database/opensearch/main.tf
@@ -20,7 +20,7 @@ resource "aws_opensearch_domain" "opensearch" {
 
   advanced_security_options {
     enabled                        = var.security_options_enabled
-    anonymous_auth_enabled         = true
+    anonymous_auth_enabled         = false
     internal_user_database_enabled = var.internal_user_database_enabled
     master_user_options {
       master_user_name     = var.master_user_name
