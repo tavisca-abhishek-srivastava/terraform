5f9036dccc0c06f7efb521a0ce96095dcff2009c
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-16 10:57:18 +0530

    added cloud watch log for opensearch

--- actual-work/modules/database/opensearch/cloud_watch_log_group.tf
+++ actual-work/modules/database/opensearch/cloud_watch_log_group.tf
@@ -16,38 +16,38 @@ resource "aws_cloudwatch_log_group" "opensearch_log_group_es_application_logs" {
 }
 
 
-resource "aws_cloudwatch_log_resource_policy" "opensearch_log_resource_policy" {
-  policy_name = "${var.open_search_domain_name}-domain-log-resource-policy"
+# resource "aws_cloudwatch_log_resource_policy" "opensearch_log_resource_policy" {
+#   policy_name = "${var.open_search_domain_name}-domain-log-resource-policy"
 
-  policy_document = <<CONFIG
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "es.amazonaws.com"
-      },
-      "Action": [
-        "logs:PutLogEvents",
-        "logs:PutLogEventsBatch",
-        "logs:CreateLogStream"
-      ],
-      "Resource": [
-        "${aws_cloudwatch_log_group.opensearch_log_group_index_slow_logs.arn}:*",
-        "${aws_cloudwatch_log_group.opensearch_log_group_search_slow_logs.arn}:*",
-        "${aws_cloudwatch_log_group.opensearch_log_group_es_application_logs.arn}:*"
-      ],
-      "Condition": {
-          "StringEquals": {
-              "aws:SourceAccount": "${data.aws_caller_identity.current.account_id}"
-          },
-          "ArnLike": {
-              "aws:SourceArn": "arn:aws:es:eu-central-1:${data.aws_caller_identity.current.account_id}:domain/${var.open_search_domain_name}"
-          }
-      }
-    }
-  ]
-}
-CONFIG
-}
+#   policy_document = <<CONFIG
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "es.amazonaws.com"
+#       },
+#       "Action": [
+#         "logs:PutLogEvents",
+#         "logs:PutLogEventsBatch",
+#         "logs:CreateLogStream"
+#       ],
+#       "Resource": [
+#         "${aws_cloudwatch_log_group.opensearch_log_group_index_slow_logs.arn}:*",
+#         "${aws_cloudwatch_log_group.opensearch_log_group_search_slow_logs.arn}:*",
+#         "${aws_cloudwatch_log_group.opensearch_log_group_es_application_logs.arn}:*"
+#       ],
+#       "Condition": {
+#           "StringEquals": {
+#               "aws:SourceAccount": "${data.aws_caller_identity.current.account_id}"
+#           },
+#           "ArnLike": {
+#               "aws:SourceArn": "arn:aws:es:eu-central-1:${data.aws_caller_identity.current.account_id}:domain/${var.open_search_domain_name}"
+#           }
+#       }
+#     }
+#   ]
+# }
+# CONFIG
+# }
