ccdc2f83a8beed0e32ffbc8eaa7fb5f3e142dd1d
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-16 11:57:28 +0530

    added cloud watch log for opensearch

--- actual-work/modules/database/opensearch/data.tf
+++ actual-work/modules/database/opensearch/data.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "opensearch_access_policy" {
 
     actions = ["es.*"]
 
-    resources = "arn:aws:es:${var.aws_region}:${data.aws_caller_identity.current.account_id}:domain/${var.open_search_domain_name}/*"
+    resources = ["arn:aws:es:${var.aws_region}:${data.aws_caller_identity.current.account_id}:domain/${var.open_search_domain_name}/*"]
   }
 }
 
