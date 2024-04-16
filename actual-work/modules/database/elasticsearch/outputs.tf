e309b15465230785b38f5a92f6da0e632b39bb04
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-16 13:51:02 +0530

    added extra output in module

--- actual-work/modules/database/elasticsearch/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "opensearch_dashboard_endpoint" {
-    value = aws_opensearch_domain.opensearch.dashboard_endpoint
-}
--- actual-work/modules/database/opensearch/outputs.tf
+++ actual-work/modules/database/opensearch/outputs.tf
@@ -1,3 +1,9 @@
 output "opensearch_dashboard_endpoint" {
     value = aws_opensearch_domain.opensearch.dashboard_endpoint
 }
+output "opensearch_domain_endpoint" {
+    value = aws_opensearch_domain.opensearch.endpoint
+}
+output "opensearch_kibana_endpoint" {
+    value = aws_opensearch_domain.opensearch.kibana_endpoint
+}
