f080231c98a3dde539d07b163fa0b826e990e28b
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-16 13:56:16 +0530

    added extra output in module

---
+++ actual-work/Examples/database/opensearch_based_elasticsearch/outputs.tf
@@ -0,0 +1,9 @@
+output "opensearch_dashboard_endpoint" {
+    value = module.opensearch.opensearch_dashboard_endpoint
+}
+output "opensearch_domain_endpoint" {
+    value = module.opensearch.opensearch_domain_endpoint
+}
+output "opensearch_kibana_endpoint" {
+    value = module.opensearch.opensearch_kibana_endpoint
+}
