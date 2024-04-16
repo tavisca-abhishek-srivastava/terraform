13662f368a8b69a990ed3c5b750e5481d179de61
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-16 12:27:11 +0530

    added cloud watch log for opensearch

--- actual-work/modules/database/opensearch/cloud_watch_log_group.tf
+++ actual-work/modules/database/opensearch/cloud_watch_log_group.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_log_group" "opensearch_log_group_index_slow_logs" {
   name              = "/aws/opensearch/${var.open_search_domain_name}/index-slow"
   retention_in_days = var.cloud_watch_log_group_retention_days
   tags = var.tags
-  kms_key_id = module.opensearch_encryption_at_rest_cmk.mrk_cms_arn
+  
 }
 
 
@@ -10,7 +10,7 @@ resource "aws_cloudwatch_log_group" "opensearch_log_group_search_slow_logs" {
   name              = "/aws/opensearch/${var.open_search_domain_name}/search-slow"
   retention_in_days = var.cloud_watch_log_group_retention_days
   tags = var.tags
-  kms_key_id = module.opensearch_encryption_at_rest_cmk.mrk_cms_arn
+  
 }
 
 
@@ -18,5 +18,5 @@ resource "aws_cloudwatch_log_group" "opensearch_log_group_es_application_logs" {
   name              = "/aws/opensearch/${var.open_search_domain_name}/es-application"
   retention_in_days = var.cloud_watch_log_group_retention_days
   tags = var.tags
-  kms_key_id = module.opensearch_encryption_at_rest_cmk.mrk_cms_arn
+  
 }
