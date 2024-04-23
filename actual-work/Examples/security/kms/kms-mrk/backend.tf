4b28c5e904a45b979700b176ad5d3f65c334eea6
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-23 15:22:47 +0530

    enhanced KMS with policy

--- actual-work/Examples/security/kms/kms-mrk/backend.tf
+++ actual-work/Examples/security/kms/kms-mrk/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tapoc-ec2-privatekey"
-    key    = "ta/remote-backend-nrt-kms"
+    key    = "ta/remote-backend-kms-mrk"
     region = "us-east-1"
   }
 }
