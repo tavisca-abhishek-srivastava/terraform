f37c2359d362931ed61dc0e59966c55d77681122
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-25 15:08:45 +0530

    added policy document module with example

--- actual-work/modules/security/iam_policies/main.tf
+++ actual-work/modules/security/iam_policies/main.tf
@@ -1,6 +1,17 @@
+# module "iam_policy_document_example" {
+#     source           = "../iam_policy_document/iam_policy_document"
+#     key_policy_statements = var.key_policy_statements
+# }
+
+module "iam_policy_document_example" {
+  source           = "../iam_policy_document/"
+  key_policy_statements = var.key_policy_statements
+
+}
+
 resource "aws_iam_policy" "policy" {
   name        = var.policy_name
   description = var.policy_description
-  policy      = var.policy_document
+  policy      = module.iam_policy_document_example.policy_document_json
   tags = var.tags
 }
