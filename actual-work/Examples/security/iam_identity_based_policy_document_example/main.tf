c59f9ff35b183beb6633cb709da38b48b74ab563
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-25 22:37:18 +0530

    added policy document module with example

--- actual-work/Examples/security/iam_identity_based_policy_document_example/main.tf
+++ actual-work/Examples/security/iam_identity_based_policy_document_example/main.tf
@@ -1,5 +1,5 @@
 module "policy1" {
-    source           = "../../../modules/security/iam_policy"
+    source           = "../../../modules/security/iam_policies"
     policy_name   = var.policy_name
     policy_description = var.policy_description
     key_policy_statements = var.key_policy_statements
