4968482dbc41866986532f4c079e13bfe786d8dd
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-25 15:08:45 +0530

    added policy document module with example

--- actual-work/modules/security/iam_policies/variables.tf
+++ actual-work/modules/security/iam_policies/variables.tf
@@ -8,10 +8,25 @@ variable "policy_description" {
   type        = string
 }
 
-variable "policy_document" {
-  description = "The IAM policy document"
-  type        = any
+# variable "policy_document" {
+#   description = "The IAM policy document"
+#   type        = any
+# }
+
+variable "key_policy_statements" {
+  description = "All the statements for the key policy"
+  type = map(object({
+    sid = string
+    actions = list(string)
+    resources = list(string)
+    effect = string
+    principals = object({
+      identifiers = list(string)
+    })
+  }))
+default = {}
 }
+
 variable "tags" {
   type = object({
     DataClassification = string
