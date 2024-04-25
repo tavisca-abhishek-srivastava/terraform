ae6169ddde0d7299333c8a3327253f2aa56d65a2
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-25 13:44:48 +0530

    added policy document module with example

--- actual-work/Examples/security/security/kms/kms-mrk-key-single-region/main.tf
+++
@@ -1,9 +0,0 @@
-module "kms-example" {
-    source = "../../../../modules/security/kms"
-    kms_alias         = var.kms_alias
-    delete_after_days = var.delete_after_days
-    key_description   = var.key_description
-    key_policy_statements = var.key_policy_statements
-    tags = var.tags
-     
-} 
