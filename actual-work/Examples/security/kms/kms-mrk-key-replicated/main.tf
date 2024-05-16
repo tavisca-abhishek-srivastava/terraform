0f078d8d1417794d7a2fd64de9e16f904602aa38
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-05-16 16:55:11 +0530

    kms provider removed from module and added in example

--- actual-work/Examples/security/kms/kms-mrk-key-replicated/main.tf
+++ actual-work/Examples/security/kms/kms-mrk-key-replicated/main.tf
@@ -1,18 +1,30 @@
-provider "aws" {
-  region = "us-west-2"
-  alias  = "replica"
-}
 
-module "kms-example" {
+
+module "kms-example-primary" {
     source = "../../../../modules/security/kms"
     kms_alias         = var.kms_alias
     delete_after_days = var.delete_after_days
     key_description   = var.key_description
     key_policy_statements = var.key_policy_statements
     need_kms_replica = var.need_kms_replica
-    replica_region = var.replica_region
+    # replica_region = var.replica_region
     replica_key_policy_statements = var.replica_key_policy_statements
     tags = var.tags
+    providers = {
+      aws= aws.primary
+     }
+     
+}
+
+module "kms-example-replica" {
+    source = "../../../../modules/security/kms"
+    kms_alias         = var.kms_alias
+    # replica_region = var.replica_region
+    delete_after_days = var.delete_after_days
+    key_description   = var.key_description
+    replica_key_policy_statements = var.replica_key_policy_statements
+    tags = var.tags
+    primary_key_arn = module.kms-example-replica.mrk_cms_arn
     providers = {
       aws= aws.replica
      }
