82ed9b76aa9695690ed7000ef28f5968d4c62d2c
tavisca-abhishek-srivastava <abhishek.srivastava@tavisca.com>
2024-05-22 11:35:57 +0530

    new opensearch with all attributes

--- actual-work/Examples/database/opensearch-instance-existing-kms-security-group/backend.tf
+++ actual-work/Examples/database/opensearch-instance-existing-kms-security-group/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tapoc-ec2-privatekey"
-    key    = "ta/remote-backend-nrt-opensearch-existing-kms"
+    key    = "ta/remote-backend-nrt-opensearch-existing-kms-2"
     region = "us-east-1"
   }
 }
