9f4384989c2ebccb806a67ec2eb940493a2be9d7
tavisca-abhishek-srivastava <abhishek.srivastava@tavisca.com>
2024-05-24 13:56:45 +0530

    autota-validation

--- actual-work/Examples/database/opensearch-instance-new-kms/main.tf
+++ actual-work/Examples/database/opensearch-instance-new-kms/main.tf
@@ -47,6 +47,6 @@ module "opensearch" {
     internal_user_database_enabled = each.value.internal_user_database_enabled
     tags = var.tags
     encrypt_at_rest_enabled = each.value.encrypt_at_rest_enabled
-    kms_key_id = module.opensearch_encryption_at_rest_cmk.mrk_cms_arn[0]
+    kms_key_id = module.opensearch_encryption_at_rest_cmk[each.key].mrk_cms_arn[0]
     security_group_ids = [module.opensearch_security_group.security_group_id]
 }
