1e72a76e9d8dfabeda006494823ffa586c78ae87
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-01-01 15:45:38 +0530

    added code for json policy based KMS and modified code of dyanmodb

--- actual-work/modules/security/kms_policy_json/variables.tf
+++ actual-work/modules/security/kms_policy_json/variables.tf
@@ -2,6 +2,7 @@ variable "kms_alias" {
   description = "define in the form of 'alias/unique_key_name'"
   type    = string
 }
+##
 
 variable "delete_after_days" {
   description = " The waiting period, specified in number of days. After the waiting period ends, AWS KMS deletes the KMS key.it must be between 7 and 30, inclusive"
