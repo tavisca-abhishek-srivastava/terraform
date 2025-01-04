810af2d1526076c4a9c9311b90abb3a90d42bdd9
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-01-04 12:38:34 +0530

    added code for json policy based KMS and modified code of dyanmodb

--- actual-work/Examples/database/dynamodb-with-autoscaling-import-table-stream-enabled/backend.tf
+++ actual-work/Examples/database/dynamodb-with-autoscaling-import-table-stream-enabled/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tapoc-ec2-privatekey"
-    key    = "ta/remote-backend-nrt-dynamodb-dest"
+    key    = "ta/remote-backend-nrt-dynamodb-dest-stream-enabled"
     region = "us-east-1"
 
 
