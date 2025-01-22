c0c8759b4c4e6b9c0cc384908a8cce973bb1cb6e
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-01-22 09:27:46 +0530

    updated kms module in RDS module code

--- actual-work/modules/security/kms_policy_json/providers.tf
+++ actual-work/modules/security/kms_policy_json/providers.tf
@@ -1,17 +1,15 @@
 terraform {
    required_providers {
-    aws = {
-    source = "hashicorp/aws"
-    version = "5.80.0"
-    configuration_aliases = [ aws.instancemaker, ]
+     aws = {
+       version = "5.80.0"
      }
    }
  }
 
-#  provider "aws" {
-#    region = var.key_primary_region
-#    alias = "primary"
-#  }
+ provider "aws" {
+   region = var.key_primary_region
+   alias = "primary"
+ }
 
 # provider "aws" {
 #       alias = "instancemaker"
