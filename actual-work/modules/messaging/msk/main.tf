e4a64cf7a8335e6add4a6ed8259a1d728beb6594
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-04-01 21:34:53 +0530

    added code for MSK

--- actual-work/modules/messaging/msk/main.tf
+++ actual-work/modules/messaging/msk/main.tf
@@ -1,10 +1,10 @@
 resource "aws_msk_cluster" "tf-s3-to-pg" {
   number_of_broker_nodes = var.number_of_broker_nodes
-  cluster_name = var.cluster_name
-  kafka_version = var.kafka_version
+  cluster_name           = var.cluster_name
+  kafka_version          = var.kafka_version
 
- broker_node_group_info {
-    instance_type = var.instance_type
+  broker_node_group_info {
+    instance_type  = var.instance_type
     client_subnets = var.client_subnets
     storage_info {
       ebs_storage_info {
@@ -17,8 +17,8 @@ resource "aws_msk_cluster" "tf-s3-to-pg" {
   encryption_info {
     encryption_at_rest_kms_key_arn = "arn:aws:kms:us-east-1:928814396842:key/0812992e-897f-41cc-b4e2-e1a2d1b3b672"
     encryption_in_transit {
-      client_broker = "PLAINTEXT"
-      in_cluster = true
+      client_broker = "TLS"
+      in_cluster    = true
     }
   }
   client_authentication {
@@ -26,20 +26,18 @@ resource "aws_msk_cluster" "tf-s3-to-pg" {
   }
 
 
-tags = {
-"AppName" = "tf-s3-to-pg"
-"Backup" = "false"
-"BusinessUnit" = "Travel.poc"
-"DataClassification" = "internal"
-"Environment" = "poc"
-"InfraOwner" = "sre-cloud-reliability@tavisca.com"
-"Name" = "msk-pg"
-"Product" = "poap"
-"Squad"  = "Travel.poc"
-  
-  
-  
-   
-}
+  tags = {
+    "AppName"            = "tf-s3-to-pg"
+    "Backup"             = "false"
+    "BusinessUnit"       = "Travel.poc"
+    "DataClassification" = "internal"
+    "Environment"        = "poc"
+    "InfraOwner"         = "sre-cloud-reliability@tavisca.com"
+    "Name"               = "msk-pg"
+    "Product"            = "poap"
+    "Squad"              = "Travel.poc"
+
+
+  }
 
 }
--- actual-work/modules/messaging/msk/variables.tf
+++ actual-work/modules/messaging/msk/variables.tf
@@ -1,34 +1,34 @@
 variable "cluster_name" {
-    description = "Name of MSK Cluster"
-    type = string
-    default = "tf-s3-to-pg"
+  description = "Name of MSK Cluster"
+  type        = string
+  default     = "tf-s3-to-pg"
 }
 variable "kafka_version" {
-    description = "Kafka Version"
-    type = string
-    default = "3.6.0"
+  description = "Kafka Version"
+  type        = string
+  default     = "3.6.0"
 }
 variable "client_subnets" {
-    description = "Subnet for brokers"
-    type = list(string)
-    default = [
-        "subnet-060048463710e54c4",
-        "subnet-01d4d19deaa34db85",
-        "subnet-061e332b24aecd27b",
-    ]
-}                                                                                                                                                              
+  description = "Subnet for brokers"
+  type        = list(string)
+  default = [
+    "subnet-060048463710e54c4",
+    "subnet-01d4d19deaa34db85",
+    "subnet-061e332b24aecd27b",
+  ]
+}
 variable "number_of_broker_nodes" {
-    description = "number of brokers in cluster"
-    type = number
-    default =   3
+  description = "number of brokers in cluster"
+  type        = number
+  default     = 3
 }
 variable "instance_type" {
-    description = "type of instance of broker"
-    type = string
-    default = "kafka.m5.large"
+  description = "type of instance of broker"
+  type        = string
+  default     = "kafka.m5.large"
 }
 variable "security_groups" {
   description = "Security group"
-  type = list(string)
-  default = ["sg-07c3c80e71d4b954e"]
+  type        = list(string)
+  default     = ["sg-07c3c80e71d4b954e"]
 }
