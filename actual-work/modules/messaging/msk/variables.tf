571337d2852e1a4dd54c50a3ea5acd7c17fa71f9
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-04-10 13:54:39 +0530

    added code for MSK

--- actual-work/modules/messaging/msk/variables.tf
+++ actual-work/modules/messaging/msk/variables.tf
@@ -11,7 +11,7 @@ variable "kafka_version" {
 variable "client_subnets" {
   description = "Subnet for brokers"
   type        = list(string)
-  #default = [    "subnet-04c8a1cce0a80f526",    "subnet-001023c820ce7e35c",    "subnet-08792a1a703950fe7"  ]
+  default = [    "subnet-04c8a1cce0a80f526",    "subnet-001023c820ce7e35c",    "subnet-08792a1a703950fe7"  ]
     validation {
       condition = length(var.client_subnets) >= 3
       error_message = "pls define atleast 3 subnets"
@@ -56,89 +56,89 @@ variable "cloud_watch_log_group_retention_days" {
 variable "key_policy_map" {
   description = "A valid policy JSON document"
   type = any
-  default = {
-    "Id" : "key-consolepolicy-3",
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "Enable IAM User Permissions",
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : "arn:aws:iam::928814396842:root"
-        },
-        "Action" : "kms:*",
-        "Resource" : "*"
-      },
-      {
-        "Sid" : "Allow access for Key Administrators",
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : [
-            "arn:aws:iam::928814396842:role/adfs-devops",
-            "arn:aws:iam::928814396842:role/adfs-governance"
-          ]
-        },
-        "Action" : [
-          "kms:Create*",
-          "kms:Describe*",
-          "kms:Enable*",
-          "kms:List*",
-          "kms:Put*",
-          "kms:Update*",
-          "kms:Revoke*",
-          "kms:Disable*",
-          "kms:Get*",
-          "kms:Delete*",
-          "kms:TagResource",
-          "kms:UntagResource",
-          "kms:ScheduleKeyDeletion",
-          "kms:CancelKeyDeletion"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Sid" : "Allow use of the key",
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : [
-            "arn:aws:iam::928814396842:role/adfs-devops",
-            "arn:aws:iam::928814396842:role/aws-service-role/kafka.amazonaws.com/AWSServiceRoleForKafka"
+  #  default = {
+  #   "Id" : "key-consolepolicy-3",
+  #   "Version" : "2012-10-17",
+  #   "Statement" : [
+  #     {
+  #       "Sid" : "Enable IAM User Permissions",
+  #       "Effect" : "Allow",
+  #       "Principal" : {
+  #         "AWS" : "arn:aws:iam::928814396842:root"
+  #       },
+  #       "Action" : "kms:*",
+  #       "Resource" : "*"
+  #     },
+  #     {
+  #       "Sid" : "Allow access for Key Administrators",
+  #       "Effect" : "Allow",
+  #       "Principal" : {
+  #         "AWS" : [
+  #           "arn:aws:iam::928814396842:role/adfs-devops",
+  #           "arn:aws:iam::928814396842:role/adfs-governance"
+  #         ]
+  #       },
+  #       "Action" : [
+  #         "kms:Create*",
+  #         "kms:Describe*",
+  #         "kms:Enable*",
+  #         "kms:List*",
+  #         "kms:Put*",
+  #         "kms:Update*",
+  #         "kms:Revoke*",
+  #         "kms:Disable*",
+  #         "kms:Get*",
+  #         "kms:Delete*",
+  #         "kms:TagResource",
+  #         "kms:UntagResource",
+  #         "kms:ScheduleKeyDeletion",
+  #         "kms:CancelKeyDeletion"
+  #       ],
+  #       "Resource" : "*"
+  #     },
+  #     {
+  #       "Sid" : "Allow use of the key",
+  #       "Effect" : "Allow",
+  #       "Principal" : {
+  #         "AWS" : [
+  #           "arn:aws:iam::928814396842:role/adfs-devops",
+  #           "arn:aws:iam::928814396842:role/aws-service-role/kafka.amazonaws.com/AWSServiceRoleForKafka"
             
-          ]
-        },
-        "Action" : [
-          "kms:Encrypt",
-          "kms:Decrypt",
-          "kms:ReEncrypt*",
-          "kms:GenerateDataKey*",
-          "kms:DescribeKey"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Sid" : "Allow attachment of persistent resources",
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : [
-            "arn:aws:iam::928814396842:role/adfs-devops",
-            "arn:aws:iam::928814396842:role/aws-service-role/kafka.amazonaws.com/AWSServiceRoleForKafka"
+  #         ]
+  #       },
+  #       "Action" : [
+  #         "kms:Encrypt",
+  #         "kms:Decrypt",
+  #         "kms:ReEncrypt*",
+  #         "kms:GenerateDataKey*",
+  #         "kms:DescribeKey"
+  #       ],
+  #       "Resource" : "*"
+  #     },
+  #     {
+  #       "Sid" : "Allow attachment of persistent resources",
+  #       "Effect" : "Allow",
+  #       "Principal" : {
+  #         "AWS" : [
+  #           "arn:aws:iam::928814396842:role/adfs-devops",
+  #           "arn:aws:iam::928814396842:role/aws-service-role/kafka.amazonaws.com/AWSServiceRoleForKafka"
            
-          ]
-        },
-        "Action" : [
-          "kms:CreateGrant",
-          "kms:ListGrants",
-          "kms:RevokeGrant"
-        ],
-        "Resource" : "*",
-        "Condition" : {
-          "Bool" : {
-            "kms:GrantIsForAWSResource" : "true"
-          }
-        }
-      }
-    ]
-  }
+  #         ]
+  #       },
+  #       "Action" : [
+  #         "kms:CreateGrant",
+  #         "kms:ListGrants",
+  #         "kms:RevokeGrant"
+  #       ],
+  #       "Resource" : "*",
+  #       "Condition" : {
+  #         "Bool" : {
+  #           "kms:GrantIsForAWSResource" : "true"
+  #         }
+  #       }
+  #     }
+  #   ]
+  # }
 }
 variable "delete_after_days" {
     description = " The waiting period, specified in number of days. After the waiting period ends, AWS KMS deletes the KMS key.it must be between 7 and 30, inclusive"
