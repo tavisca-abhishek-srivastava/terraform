1210e2936524d15b9a613969ce8fa4f800c566a7
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-04-16 14:07:24 +0530

    added code for pgsql aurora

---
+++ actual-work/modules/database/rds/pgsql/pgsql_single_region/kms.tf
@@ -0,0 +1,10 @@
+data "aws_caller_identity" "current" {}
+
+module "rds_encryption_at_rest_cmk" {
+  	source            = "../../../../security/kms_policy_json"
+  	kms_alias         = "alias/${var.cluster_identifier}_key"
+    delete_after_days = var.delete_after_days
+  	key_description   =  "Key for ${var.cluster_identifier} KMS Cluster "
+  	key_policy_map = var.key_policy_map
+	kms_tags = var.tags  
+}
---
+++ actual-work/modules/database/rds/pgsql/pgsql_single_region/main.tf
@@ -0,0 +1,16 @@
+resource "aws_rds_cluster" "name" {
+    engine = "aurora-postgresql"
+    availability_zones = ["us-east-1a"]
+    database_name = "nrt-ui"
+    cluster_identifier = var.nrt-compliance
+    master_username = "dbadmin"
+    manage_master_user_password = "welcome$1234"
+    db_cluster_instance_class = "db.r6gd.xlarge"
+    storage_type              = "io1"
+    allocated_storage         = 100
+    iops                      = 1000
+
+
+
+
+}
