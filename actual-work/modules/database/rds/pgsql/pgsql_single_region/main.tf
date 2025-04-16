7c90f19763012642e66efac4d82e20fa1bdde1cc
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-04-16 14:15:48 +0530

    added code for pgsql aurora

--- actual-work/modules/database/rds/pgsql/pgsql_single_region/main.tf
+++ actual-work/modules/database/rds/pgsql/pgsql_single_region/main.tf
@@ -1,10 +1,11 @@
 resource "aws_rds_cluster" "name" {
     engine = "aurora-postgresql"
-    availability_zones = ["us-east-1a","us-east-1b"]
+    availability_zones = ["us-east-1a","us-east-1b","us-east-1c"]
     database_name = "nrt-ui"
     cluster_identifier = var.cluster_identifier
     master_username = "dbadmin"
-    manage_master_user_password = "welcome$1234"
+    master_password = "welcome$1234"
+
     db_cluster_instance_class = "db.r6gd.xlarge"
     storage_type              = "io1"
     allocated_storage         = 100
