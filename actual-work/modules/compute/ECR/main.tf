30bc8913687fb8304a2daa350dd53f4838fba8f0
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-04-25 10:37:39 +0530

    added code for ecr repo

--- actual-work/modules/compute/ECR/main.tf
+++ actual-work/modules/compute/ECR/main.tf
@@ -5,4 +5,9 @@ resource "aws_ecr_repository" "nrt-pg-repo" {
   image_scanning_configuration {
     scan_on_push = false
   }
+  tags = {
+      InfraOwner : "sre-cloud-reliability@tavisca.com"
+      Product : "poap"
+      Name : "atlas/pg"
+  }
 }
