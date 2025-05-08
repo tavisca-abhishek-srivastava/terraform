64eeb1e1c4ab41a5d42990b0bb2184d23024fb2d
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-05-08 16:40:48 +0530

    Added code for ecs task definition

---
+++ actual-work/modules/compute/ecs/ecs_task_definition/main.tf
@@ -0,0 +1,40 @@
+resource "aws_ecs_task_definition" "name" {
+  family = "pgsql-app-task-definition-tf"
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"
+  container_definitions = jsonencode([
+    {
+      "name": "app-pgsql-task-definition-tf",
+      image     = "928814396842.dkr.ecr.us-east-1.amazonaws.com/atlas/pg:latest",
+      cpu       = 1024,
+      memory    = 3072,
+      essential = true,
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+        }
+      ],
+
+
+      
+    },
+     ])
+     runtime_platform {
+        operating_system_family = "LINUX"
+        cpu_architecture        = "X86_64"
+     }
+     tags = {
+        "AppName"            = "app-pgsql-task-definition-tf"
+        "Backup"             = "false"
+        "BusinessUnit"       = "travel.poc"
+        "DataClassification" = "internal"
+        "Environment"        = "poc"
+        "InfraOwner"         = "sre-cloud-reliability@tavisca.com"
+        "Name"               = "pgsql_node_service"
+        "ProductOwner"       = "sre-cloud-reliability@tavisca.com"
+        "Product"            = "poap"
+        "Squad"              = "travel.poc"
+      } 
+
+}
