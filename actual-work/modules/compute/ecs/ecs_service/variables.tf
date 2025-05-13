a201862f286b2e7124e074d736582e7206e9d1a4
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-05-13 15:13:40 +0530

    modified code of task  definition

--- actual-work/modules/compute/ecs/ecs_service/variables.tf
+++ actual-work/modules/compute/ecs/ecs_service/variables.tf
@@ -46,6 +46,7 @@ variable "security_groups" {
 variable "family" {
   default = "pgsql-app-task-definition-tf"
   type    = string
+  description = "A unique name for your task definition"
 }
 
 variable "requires_compatibilities" {
@@ -60,15 +61,16 @@ variable "network_mode" {
 }
 
 variable "cpu" {
-  default = 1024
+  default = 3072
   type    = number
+  description = "Number of cpu units used by the task. If the requires_compatibilities is FARGATE this field is required."
 
 }
 
 variable "memory" {
-  default = 3072
+  default = 10000
   type    = number
-
+  description = "value"
 }
 
 variable "execution_role_arn" {
@@ -88,12 +90,12 @@ variable "cpu_architecture" {
 variable "container_definitions" {
   description = "A valid JSON container definition for task"
   type        = any
-  default = [{
-    "name"      = "app-pgsql-task-definition-tf",
+  default = [{ 
+    "name"      = "app_pgsql_tf_container",   #The name of a container. If you're linking multiple containers together in a task definition, the name of one container can be entered in the links of another container to connect the containers. Up to 255 letters (uppercase and lowercase), numbers, underscores, and hyphens are allowed. This parameter maps to name in the docker container create command and the --name option to docker run.
     "image"     = "928814396842.dkr.ecr.us-east-1.amazonaws.com/atlas/pg:latest",
-    "cpu"       = 1024,
-    "memory"    = 3072,
-    "essential" = true,
+    "cpu"       = 1024,   # The number of cpu units reserved for the container. This parameter maps to CpuShares in the docker container create commandand the --cpu-shares option to docker run. This field is optional for tasks using the Fargate launch type, and the only requirement is that the total amount of CPU reserved for all containers within a task be lower than the task-level cpu value.
+    "memory"    = 3072,   #The amount (in MiB) of memory to present to the container. If your container attempts to exceed the memory specified here, the container is killed. The total amount of memory reserved for all containers within a task must be lower than the task memory value, if one is specified. This parameter maps to Memory in the docker container create command and the --memory option to docker run.
+    "essential" = true,  #If the essential parameter of a container is marked as true, and that container fails or stops for any reason, all other containers that are part of the task are stopped. If the essential parameter of a container is marked as false, its failure doesn't affect the rest of the containers in a task. If this parameter is omitted, a container is assumed to be essential.
     "portMappings" = [
       {
         "containerPort" = 8000
