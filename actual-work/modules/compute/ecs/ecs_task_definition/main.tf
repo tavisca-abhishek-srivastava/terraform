52d3566d18486279515e6b13b37443e162811bf4
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-05-28 22:47:29 +0530

    added new task definition for multi container app

--- actual-work/modules/compute/ecs/ecs_task_definition/main.tf
+++ actual-work/modules/compute/ecs/ecs_task_definition/main.tf
@@ -6,10 +6,7 @@ resource "aws_ecs_task_definition" "aws_ecs_task_definition_tf" {
   memory = var.memory
   execution_role_arn = var.execution_role_arn
   container_definitions = jsonencode(var.container_definitions)
-  volume {
-                    name      = "mongo-db"
-                    host_path = "/data/db"
-  }
+
   runtime_platform {
         operating_system_family = var.operating_system_family
         cpu_architecture        = var.cpu_architecture
