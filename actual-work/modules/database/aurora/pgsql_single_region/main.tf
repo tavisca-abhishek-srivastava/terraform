c6744fd00887931958b503764fe2ec1adc6ed99a
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-07-28 12:31:38 +0530

    added code for aurora

--- actual-work/modules/database/aurora/pgsql_single_region/main.tf
+++ actual-work/modules/database/aurora/pgsql_single_region/main.tf
@@ -22,4 +22,6 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
   instance_class        =   "db.r4.large"
   engine                =   aws_rds_cluster.default.engine
   engine_version        =   aws_rds_cluster.default.engine_version
+  db_subnet_group_name = "bnr-data-subnet-grp"
+  tags = var.tags
 }
