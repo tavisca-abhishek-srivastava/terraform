f7aa8105dee3e680f3839ef6c369aeacea0887d7
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-08-14 13:43:01 +0530

    added cdc validation apig

--- actual-work/modules/database/aurora/pgsql_global_database/main.tf
+++ actual-work/modules/database/aurora/pgsql_global_database/main.tf
@@ -20,7 +20,7 @@ resource "aws_rds_cluster" "postgresql" {
   #preferred_backup_window = "07:00-09:00"
   skip_final_snapshot = true
   apply_immediately = true
-  kms_key_id = "arn:aws:kms:us-east-1:346319152574:key/mrk-4eca0415b8b4434f96399c2f2d80b1ef"
+  kms_key_id = module.rds_encryption_at_rest_cmk.mrk_cms_arn[1].arn
   storage_encrypted = true
 
   db_subnet_group_name = "bnr-data-subnet-grp"
@@ -46,7 +46,7 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
   db_parameter_group_name = module.nrt_rds_parameter_group.parameter_group_name
   promotion_tier = each.value.promotion_tier
   performance_insights_enabled = true
-  performance_insights_kms_key_id = "arn:aws:kms:us-east-1:346319152574:key/mrk-4eca0415b8b4434f96399c2f2d80b1ef"
+  performance_insights_kms_key_id = module.rds_encryption_at_rest_cmk.mrk_cms_arn[1].arn #"arn:aws:kms:us-east-1:346319152574:key/mrk-4eca0415b8b4434f96399c2f2d80b1ef"
   performance_insights_retention_period = 7
   
   tags = var.tags
