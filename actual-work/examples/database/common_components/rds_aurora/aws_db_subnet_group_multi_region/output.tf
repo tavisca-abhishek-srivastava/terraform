70f030d41130b49b041b333a299544625493641b
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-08-16 16:29:47 +0530

    added cdc validation apig

--- actual-work/examples/database/common_components/rds_aurora/aws_db_subnet_group_multi_region/output.tf
+++ actual-work/examples/database/common_components/rds_aurora/aws_db_subnet_group_multi_region/output.tf
@@ -1,7 +1,7 @@
 output "name_of_primary_subnet" {
-  value = module.nrt_rds_subnet_group["1"].subnet_name.name_of_primary_subnet
+  value = module.nrt_rds_subnet_group["1"].subnet_name
 }
 
 output "name_of_replica_subnet" {
-  value = module.nrt_rds_subnet_group_or["1"].subnet_name.name_of_primary_subnet
+  value = module.nrt_rds_subnet_group_or["1"].subnet_name
 }
