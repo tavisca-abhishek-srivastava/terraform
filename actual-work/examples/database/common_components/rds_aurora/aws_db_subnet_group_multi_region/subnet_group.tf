5d313d1ee53a54d25627f991063b4ef30de6eda0
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-08-16 15:43:46 +0530

    added cdc validation apig

--- actual-work/examples/database/common_components/rds_aurora/aws_db_subnet_group_multi_region/subnet_group.tf
+++ actual-work/examples/database/common_components/rds_aurora/aws_db_subnet_group_multi_region/subnet_group.tf
@@ -1,5 +1,5 @@
 module "nrt_rds_subnet_group" {
-	  source = "../../../../modules/database/common_components/rds_aurora/aws_db_subnet_group_multi_region/"
+	  source = "../../../../../modules/database/common_components/rds_aurora/aws_db_subnet_group_multi_region/"
 	  ### if var.use_default_subnet_group == false then it will create new subnet group else will use default subnet group provided by user
 	  for_each = (var.use_default_subnet_group == false) ? toset(["1"]):toset([])
 	  subnet_group_name = "testing-subnet-group"
@@ -12,7 +12,7 @@ module "nrt_rds_subnet_group" {
 }
 
 module "nrt_rds_subnet_group_or" {
-	  source = "../../../../modules/database/common_components/rds_aurora/aws_db_subnet_group_multi_region/"
+	  source = "../../../../../modules/database/common_components/rds_aurora/aws_db_subnet_group_multi_region/"
 	  ### if var.use_default_subnet_group == false then it will create new subnet group else will use default subnet group provided by user
 	  for_each = (var.use_default_subnet_group == false) ? toset(["1"]):toset([])
 	  subnet_group_name = "testing-subnet-group"
