4da61784f09ed4a30ad694a4c1fd4953ff194fe0
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-08-16 20:58:21 +0530

    added cdc validation apig

--- actual-work/modules/database/common_components/rds_aurora/aws_db_parameter_group/outputs.tf
+++ actual-work/modules/database/common_components/rds_aurora/aws_db_parameter_group/outputs.tf
@@ -1,6 +1,3 @@
 	output "parameter_group_name" {
 	value = aws_db_parameter_group.parameter_group_for_db.id
 	 }
-	output "parameter_group_name_or" {
-	value = aws_db_parameter_group.parameter_group_for_db_or.id
-	 }
